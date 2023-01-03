45c920c7a438080999903b9b16b0015502dd66f3
Justin Radatti <jdradatti@gmail.com>
2023-01-02 16:26:36 -0800

    Added m1 support

--- params.sh
+++ params.sh
@@ -1,7 +1,8 @@
 export DOCKER_TAG=latest
 export GIT_REPO=https://github.com/rdkit/rdkit.git
 export GIT_BRANCH=master
+export TARGET_ARCH=linux/arm64 #linux/amd64
 unset GIT_TAG
 
-export BASE=informaticsmatters
+export BASE=jdradatti
 echo "DOCKER_TAG=$DOCKER_TAG GIT_BRANCH=$GIT_BRANCH no tag"
